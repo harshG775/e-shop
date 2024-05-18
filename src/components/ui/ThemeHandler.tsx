508565856f2682ac12cbf8f640d6dc6d034c9235
Harsh <hgaur491@gmail.com>
2024-05-18 17:36:55 +0530

    adding theme changer icon and fixing to bottom right

--- src/components/ui/ThemeHandler.tsx
+++ src/components/ui/ThemeHandler.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { setCookie } from "cookies-next";
-import { useEffect, useState } from "react";
-
-
-
-
-export default function ThemeHandler({currentTheme}: {currentTheme: string;}) {
+import { useState } from "react";
+import Icon from "../icons/lucide";
+export default function ThemeHandler({
+    currentTheme,
+}: {
+    currentTheme: string;
+}) {
     const [cTheme, setCTheme] = useState(currentTheme);
-    
+
     const handleTheme = () => {
-        const newTheme = cTheme === "light" ? "dark" : "light"
+        const newTheme = cTheme === "light" ? "dark" : "light";
         document.documentElement.setAttribute("data-theme", newTheme);
         setCookie("theme", newTheme, { maxAge: 30 * 24 * 60 * 60, path: "/" });
         setCTheme(newTheme);
     };
 
-    return <button onClick={handleTheme}>Theme toggle</button>;
+    return (
+        <button className="fixed bottom-4 right-4 p-2 text-primary rounded-full hover:bg-primary hover:text-neutral-50" onClick={handleTheme}>
+            <Icon
+                name={cTheme === "light" ? "Moon" : "Sun"}
+                className="w-6 h-6"
+            />
+        </button>
+    );
 }
