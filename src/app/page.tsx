389a59f1c60a3f8de4cd41c7af32602f7a63787e
Harsh <hgaur491@gmail.com>
2024-05-18 17:53:48 +0530

    creating root welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main>
-        <div className="text-3xl bg-primary text-txt">Theme</div>
-    </main>
-  );
+    return (
+        <main className="h-dvh w-dvw grid place-content-center gap-4">
+            <div className="text-4xl font-semibold">
+                Welcome 
+                <br className="sm:hidden"/>
+                to
+                <span className="text-primary font-bold"> EShop</span>
+            </div>
+            <div className="text-center py-1">
+                <Link
+                    href="/home"
+                    className="text-2xl bg-primary text-neutral-50 rounded-md px-4 py-2"
+                >
+                    Go to Home
+                </Link>
+            </div>
+        </main>
+    );
 }
