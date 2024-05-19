d39172c7b5cf5c69f03aefd85bbd0da37039ab5b
Harsh <hgaur491@gmail.com>
2024-05-19 17:29:10 +0530

    BannerSection_1

--- src/app/(E_Shop)/home/page.tsx
+++ src/app/(E_Shop)/home/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import BannerSection_1 from "@/components/pagesComponents/home/bannerSection_1/BannerSection_1";
 import FeaturedProducts from "@/components/pagesComponents/home/featuredProducts/FeaturedProducts";
 import Hero from "@/components/pagesComponents/home/header/hero/Hero"
 import HeroBottom from "@/components/pagesComponents/home/header/heroBottom/HeroBottom";
@@ -98,6 +99,7 @@ const HomePageData = {
             left:{
                 title:"free shipping everywhere",
                 description: "free shipping on all orders",
+                background: "https://as1.ftcdn.net/v2/jpg/02/37/91/58/1000_F_237915853_g8plRpm5VflNr9dHtLqNH6T2DpRSJyQp.jpg",
                 callToAction:[
                     {
                         title: "Shop Now",
@@ -108,6 +110,7 @@ const HomePageData = {
             right:{
                 title: "Black Friday Clearance",
                 description: "Up to 50% off",
+                background: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=799&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 callToAction:[
                     {
                         title: "Shop Now",
@@ -293,6 +296,7 @@ export default function Home() {
                 <HeroBottom data={HomePageData.header.heroBottom}/>
             </header>
             <FeaturedProducts data={HomePageData.sections.section_1}/>
+            <BannerSection_1 data={HomePageData.sections.bannerSection_1}/>
         </main>
     );
 }
---
+++ src/components/pagesComponents/home/bannerSection_1/BannerSection_1.tsx
@@ -0,0 +1,57 @@
+import CallToAction from "@/components/ui/CallToAction";
+
+type BannerSection_1Type = {
+    title: string | null;
+    left: {
+        title: string;
+        description: string;
+        background: string;
+        callToAction: {
+            title: string;
+            link: string;
+        }[];
+    };
+    right: {
+        title: string;
+        description: string;
+        background: string;
+        callToAction: {
+            title: string;
+            link: string;
+        }[];
+    };
+};
+type BannerSection_1Props = {
+    data: BannerSection_1Type;
+};
+export default function BannerSection_1({ data }: BannerSection_1Props) {
+    const { left, right } = data;
+    return (
+        <section>
+            <div
+                className="
+                    max-w-8xl mx-auto p-4 lg:p-8 transition-[padding] duration-300 delay-100 ease-in-out
+                    grid lg:grid-cols-[3fr_2fr] gap-4
+                "
+            >
+                <div
+                    style={{ backgroundImage: `url(${left.background})` , backgroundSize: "cover"}}
+                    className="min-h-96 rounded-xl p-10 flex flex-col justify-start gap-4 items-end"
+                >
+                    <h2 className="text-4xl font-semibold text-primary-500">{left.title}</h2>
+                    <p className="text-3xl text-neutral-900">{left.description}</p>
+                    <CallToAction callToAction={left.callToAction} />
+                </div>
+                <div
+                    style={{ backgroundImage: `url(${right.background})`, backgroundSize: "cover"}}
+                    className="min-h-96 rounded-xl p-10 flex flex-col justify-start gap-4 "
+                >
+                    <h2 className="text-4xl font-semibold text-primary-500">{right.title}</h2>
+                    <p className="text-3xl text-neutral-500">{right.description}</p>
+                    <CallToAction callToAction={right.callToAction} />
+                </div>
+            </div>
+            <div className="min-h-screen"></div>
+        </section>
+    );
+}
